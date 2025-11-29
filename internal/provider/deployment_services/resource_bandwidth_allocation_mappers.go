a6e26e9a0af878702945426084aa8b0dcc5781e3
vnarayanan <vnarayanan@paloaltonetworks.com>
2025-11-29 14:52:48 -0800

    - Bandwidth allocation template, examples added, spec defaults added - some framework changes (list data source now interates the list model instead of assuming folder, snippet, device) - resource template now takes into account delete query parameters for things like spn list in bandwidth allocation delete

--- internal/provider/deployment_services/resource_bandwidth_allocation_mappers.go
+++ internal/provider/deployment_services/resource_bandwidth_allocation_mappers.go
@@ -1,12 +1,7 @@
 package provider
 
-/*
-
 import (
 	"context"
-	"fmt"
-	"reflect"
-	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -14,119 +9,110 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 
-	models "github.com/paloaltonetworks/terraform-provider-scm/internal/models/deployment_services"
 	"github.com/paloaltonetworks/scm-go/generated/deployment_services"
+	models "github.com/paloaltonetworks/terraform-provider-scm/internal/models/deployment_services"
 )
-
-
-
-
-
-
-
-
-
 
 // --- Unpacker for BandwidthAllocations ---
 func unpackBandwidthAllocationsToSdk(ctx context.Context, obj types.Object) (*deployment_services.BandwidthAllocations, diag.Diagnostics) {
-    tflog.Debug(ctx, "Entering unpack helper for models.BandwidthAllocations", map[string]interface{}{"tf_object": obj})
-    diags := diag.Diagnostics{}
-    var model models.BandwidthAllocations
-    diags.Append(obj.As(ctx, &model, basetypes.ObjectAsOptions{})...)
-    if diags.HasError() {
-        tflog.Error(ctx, "Error converting Terraform object to Go model", map[string]interface{}{"diags": diags})
-        return nil, diags
-    }
+	tflog.Debug(ctx, "Entering unpack helper for models.BandwidthAllocations", map[string]interface{}{"tf_object": obj})
+	diags := diag.Diagnostics{}
+	var model models.BandwidthAllocations
+	diags.Append(obj.As(ctx, &model, basetypes.ObjectAsOptions{})...)
+	if diags.HasError() {
+		tflog.Error(ctx, "Error converting Terraform object to Go model", map[string]interface{}{"diags": diags})
+		return nil, diags
+	}
 	tflog.Debug(ctx, "Successfully converted Terraform object to Go model")
 
-    var sdk deployment_services.BandwidthAllocations
-    var d diag.Diagnostics
+	var sdk deployment_services.BandwidthAllocations
+	var d diag.Diagnostics
 
-    // Handling Primitives
-    if !model.AllocatedBandwidth.IsNull() && !model.AllocatedBandwidth.IsUnknown() {
-        sdk.AllocatedBandwidth = int32(model.AllocatedBandwidth.ValueInt64())
-        tflog.Debug(ctx, "Unpacked primitive value", map[string]interface{}{"field": "AllocatedBandwidth", "value": sdk.AllocatedBandwidth})
-    }
+	// Handling Primitives
+	if !model.AllocatedBandwidth.IsNull() && !model.AllocatedBandwidth.IsUnknown() {
+		sdk.AllocatedBandwidth = int32(model.AllocatedBandwidth.ValueInt64())
+		tflog.Debug(ctx, "Unpacked primitive value", map[string]interface{}{"field": "AllocatedBandwidth", "value": sdk.AllocatedBandwidth})
+	}
 
-    // Handling Primitives
-    if !model.Name.IsNull() && !model.Name.IsUnknown() {
-        sdk.Name = model.Name.ValueString()
-        tflog.Debug(ctx, "Unpacked primitive value", map[string]interface{}{"field": "Name", "value": sdk.Name})
-    }
+	// Handling Primitives
+	if !model.Name.IsNull() && !model.Name.IsUnknown() {
+		sdk.Name = model.Name.ValueString()
+		tflog.Debug(ctx, "Unpacked primitive value", map[string]interface{}{"field": "Name", "value": sdk.Name})
+	}
 
-    // Handling Objects
-    if !model.Qos.IsNull() && !model.Qos.IsUnknown() {
-        tflog.Debug(ctx, "Unpacking nested object for field Qos")
-        unpacked, d := unpackBandwidthAllocationsQosToSdk(ctx, model.Qos)
-        diags.Append(d...)
+	// Handling Objects
+	if !model.Qos.IsNull() && !model.Qos.IsUnknown() {
+		tflog.Debug(ctx, "Unpacking nested object for field Qos")
+		unpacked, d := unpackBandwidthAllocationsQosToSdk(ctx, model.Qos)
+		diags.Append(d...)
 		if d.HasError() {
 			tflog.Error(ctx, "Error unpacking nested object", map[string]interface{}{"field": "Qos"})
 		}
-        if unpacked != nil {
-            sdk.Qos = unpacked
-        }
-    }
+		if unpacked != nil {
+			sdk.Qos = unpacked
+		}
+	}
 
-    // Handling Lists
-    if !model.SpnNameList.IsNull() && !model.SpnNameList.IsUnknown() {
-        tflog.Debug(ctx, "Unpacking list of primitives for field SpnNameList")
-        diags.Append(model.SpnNameList.ElementsAs(ctx, &sdk.SpnNameList, false)...)
-    }
+	// Handling Lists
+	if !model.SpnNameList.IsNull() && !model.SpnNameList.IsUnknown() {
+		tflog.Debug(ctx, "Unpacking list of primitives for field SpnNameList")
+		diags.Append(model.SpnNameList.ElementsAs(ctx, &sdk.SpnNameList, false)...)
+	}
 
-diags.Append(d...)
+	diags.Append(d...)
 
-    tflog.Debug(ctx, "Exiting unpack helper for models.BandwidthAllocations", map[string]interface{}{"has_errors": diags.HasError()})
-    return &sdk, diags
+	tflog.Debug(ctx, "Exiting unpack helper for models.BandwidthAllocations", map[string]interface{}{"has_errors": diags.HasError()})
+	return &sdk, diags
 
 }
 
 // --- Packer for BandwidthAllocations ---
 func packBandwidthAllocationsFromSdk(ctx context.Context, sdk deployment_services.BandwidthAllocations) (types.Object, diag.Diagnostics) {
-    tflog.Debug(ctx, "Entering pack helper for models.BandwidthAllocations", map[string]interface{}{"sdk_struct": sdk})
-    diags := diag.Diagnostics{}
-    var model models.BandwidthAllocations
-    var d diag.Diagnostics
-    // Handling Primitives
-    // Standard primitive packing
-    model.AllocatedBandwidth = basetypes.NewInt64Value(int64(sdk.AllocatedBandwidth))
-    tflog.Debug(ctx, "Packed primitive value", map[string]interface{}{"field": "AllocatedBandwidth", "value": sdk.AllocatedBandwidth})
-    // Handling Primitives
-    // Standard primitive packing
-    model.Name = basetypes.NewStringValue(sdk.Name)
-    tflog.Debug(ctx, "Packed primitive value", map[string]interface{}{"field": "Name", "value": sdk.Name})
-    // Handling Objects
-    // This is a regular nested object that has its own packer.
-    if sdk.Qos != nil {
-        tflog.Debug(ctx, "Packing nested object for field Qos")
-        packed, d := packBandwidthAllocationsQosFromSdk(ctx, *sdk.Qos)
-        diags.Append(d...)
+	tflog.Debug(ctx, "Entering pack helper for models.BandwidthAllocations", map[string]interface{}{"sdk_struct": sdk})
+	diags := diag.Diagnostics{}
+	var model models.BandwidthAllocations
+	var d diag.Diagnostics
+	// Handling Primitives
+	// Standard primitive packing
+	model.AllocatedBandwidth = basetypes.NewInt64Value(int64(sdk.AllocatedBandwidth))
+	tflog.Debug(ctx, "Packed primitive value", map[string]interface{}{"field": "AllocatedBandwidth", "value": sdk.AllocatedBandwidth})
+	// Handling Primitives
+	// Standard primitive packing
+	model.Name = basetypes.NewStringValue(sdk.Name)
+	tflog.Debug(ctx, "Packed primitive value", map[string]interface{}{"field": "Name", "value": sdk.Name})
+	// Handling Objects
+	// This is a regular nested object that has its own packer.
+	if sdk.Qos != nil {
+		tflog.Debug(ctx, "Packing nested object for field Qos")
+		packed, d := packBandwidthAllocationsQosFromSdk(ctx, *sdk.Qos)
+		diags.Append(d...)
 		if d.HasError() {
 			tflog.Error(ctx, "Error packing nested object", map[string]interface{}{"field": "Qos"})
 		}
-        model.Qos = packed
-    } else {
-        model.Qos = basetypes.NewObjectNull(models.BandwidthAllocationsQos{}.AttrTypes())
-    }
-    // Handling Lists
-    if sdk.SpnNameList != nil {
-        tflog.Debug(ctx, "Packing list of primitives for field SpnNameList")
-        var d diag.Diagnostics
-        // This logic now dynamically determines the element type based on the SDK's Go type.
-        var elemType attr.Type = basetypes.StringType{} // Default to string
-        model.SpnNameList, d = basetypes.NewListValueFrom(ctx, elemType, sdk.SpnNameList)
-        diags.Append(d...)
-    } else {
-        // This logic now creates a correctly typed null list.
-        var elemType attr.Type = basetypes.StringType{} // Default to string
-        model.SpnNameList = basetypes.NewListNull(elemType)
-    }
-diags.Append(d...)
+		model.Qos = packed
+	} else {
+		model.Qos = basetypes.NewObjectNull(models.BandwidthAllocationsQos{}.AttrTypes())
+	}
+	// Handling Lists
+	if sdk.SpnNameList != nil {
+		tflog.Debug(ctx, "Packing list of primitives for field SpnNameList")
+		var d diag.Diagnostics
+		// This logic now dynamically determines the element type based on the SDK's Go type.
+		var elemType attr.Type = basetypes.StringType{} // Default to string
+		model.SpnNameList, d = basetypes.NewListValueFrom(ctx, elemType, sdk.SpnNameList)
+		diags.Append(d...)
+	} else {
+		// This logic now creates a correctly typed null list.
+		var elemType attr.Type = basetypes.StringType{} // Default to string
+		model.SpnNameList = basetypes.NewListNull(elemType)
+	}
+	diags.Append(d...)
 
-    obj, d := types.ObjectValueFrom(ctx, models.BandwidthAllocations{}.AttrTypes(), &model)
+	obj, d := types.ObjectValueFrom(ctx, models.BandwidthAllocations{}.AttrTypes(), &model)
 	tflog.Debug(ctx, "Final object to be returned from pack helper", map[string]interface{}{"object": obj})
-    diags.Append(d...)
-    tflog.Debug(ctx, "Exiting pack helper for models.BandwidthAllocations", map[string]interface{}{"has_errors": diags.HasError()})
-    return obj, diags
+	diags.Append(d...)
+	tflog.Debug(ctx, "Exiting pack helper for models.BandwidthAllocations", map[string]interface{}{"has_errors": diags.HasError()})
+	return obj, diags
 
 }
 
@@ -178,95 +164,95 @@ func packBandwidthAllocationsListFromSdk(ctx context.Context, sdks []deployment_
 
 // --- Unpacker for BandwidthAllocationsQos ---
 func unpackBandwidthAllocationsQosToSdk(ctx context.Context, obj types.Object) (*deployment_services.BandwidthAllocationsQos, diag.Diagnostics) {
-    tflog.Debug(ctx, "Entering unpack helper for models.BandwidthAllocationsQos", map[string]interface{}{"tf_object": obj})
-    diags := diag.Diagnostics{}
-    var model models.BandwidthAllocationsQos
-    diags.Append(obj.As(ctx, &model, basetypes.ObjectAsOptions{})...)
-    if diags.HasError() {
-        tflog.Error(ctx, "Error converting Terraform object to Go model", map[string]interface{}{"diags": diags})
-        return nil, diags
-    }
+	tflog.Debug(ctx, "Entering unpack helper for models.BandwidthAllocationsQos", map[string]interface{}{"tf_object": obj})
+	diags := diag.Diagnostics{}
+	var model models.BandwidthAllocationsQos
+	diags.Append(obj.As(ctx, &model, basetypes.ObjectAsOptions{})...)
+	if diags.HasError() {
+		tflog.Error(ctx, "Error converting Terraform object to Go model", map[string]interface{}{"diags": diags})
+		return nil, diags
+	}
 	tflog.Debug(ctx, "Successfully converted Terraform object to Go model")
 
-    var sdk deployment_services.BandwidthAllocationsQos
-    var d diag.Diagnostics
-    // Handling Primitives
-    if !model.Customized.IsNull() && !model.Customized.IsUnknown() {
-        sdk.Customized = model.Customized.ValueBoolPointer()
-        tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "Customized", "value": *sdk.Customized})
-    }
+	var sdk deployment_services.BandwidthAllocationsQos
+	var d diag.Diagnostics
+	// Handling Primitives
+	if !model.Customized.IsNull() && !model.Customized.IsUnknown() {
+		sdk.Customized = model.Customized.ValueBoolPointer()
+		tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "Customized", "value": *sdk.Customized})
+	}
 
-    // Handling Primitives
-    if !model.Enabled.IsNull() && !model.Enabled.IsUnknown() {
-        sdk.Enabled = model.Enabled.ValueBoolPointer()
-        tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "Enabled", "value": *sdk.Enabled})
-    }
+	// Handling Primitives
+	if !model.Enabled.IsNull() && !model.Enabled.IsUnknown() {
+		sdk.Enabled = model.Enabled.ValueBoolPointer()
+		tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "Enabled", "value": *sdk.Enabled})
+	}
 
-    // Handling Primitives
-    if !model.GuaranteedRatio.IsNull() && !model.GuaranteedRatio.IsUnknown() {
-        val := float32(model.GuaranteedRatio.ValueFloat64())
-        sdk.GuaranteedRatio = &val
-        tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "GuaranteedRatio", "value": *sdk.GuaranteedRatio})
-    }
+	// Handling Primitives
+	if !model.GuaranteedRatio.IsNull() && !model.GuaranteedRatio.IsUnknown() {
+		val := float32(model.GuaranteedRatio.ValueFloat64())
+		sdk.GuaranteedRatio = &val
+		tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "GuaranteedRatio", "value": *sdk.GuaranteedRatio})
+	}
 
-    // Handling Primitives
-    if !model.Profile.IsNull() && !model.Profile.IsUnknown() {
-        sdk.Profile = model.Profile.ValueStringPointer()
-        tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "Profile", "value": *sdk.Profile})
-    }
+	// Handling Primitives
+	if !model.Profile.IsNull() && !model.Profile.IsUnknown() {
+		sdk.Profile = model.Profile.ValueStringPointer()
+		tflog.Debug(ctx, "Unpacked primitive pointer", map[string]interface{}{"field": "Profile", "value": *sdk.Profile})
+	}
 
-diags.Append(d...)
+	diags.Append(d...)
 
-    tflog.Debug(ctx, "Exiting unpack helper for models.BandwidthAllocationsQos", map[string]interface{}{"has_errors": diags.HasError()})
-    return &sdk, diags
+	tflog.Debug(ctx, "Exiting unpack helper for models.BandwidthAllocationsQos", map[string]interface{}{"has_errors": diags.HasError()})
+	return &sdk, diags
 
 }
 
 // --- Packer for BandwidthAllocationsQos ---
 func packBandwidthAllocationsQosFromSdk(ctx context.Context, sdk deployment_services.BandwidthAllocationsQos) (types.Object, diag.Diagnostics) {
-    tflog.Debug(ctx, "Entering pack helper for models.BandwidthAllocationsQos", map[string]interface{}{"sdk_struct": sdk})
-    diags := diag.Diagnostics{}
-    var model models.BandwidthAllocationsQos
-    var d diag.Diagnostics
-    // Handling Primitives
-    // Standard primitive packing
-    if sdk.Customized != nil {
-        model.Customized = basetypes.NewBoolValue(*sdk.Customized)
-        tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "Customized", "value": *sdk.Customized})
-    } else {
-        model.Customized = basetypes.NewBoolNull()
-    }
-    // Handling Primitives
-    // Standard primitive packing
-    if sdk.Enabled != nil {
-        model.Enabled = basetypes.NewBoolValue(*sdk.Enabled)
-        tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "Enabled", "value": *sdk.Enabled})
-    } else {
-        model.Enabled = basetypes.NewBoolNull()
-    }
-    // Handling Primitives
-    // Standard primitive packing
-    if sdk.GuaranteedRatio != nil {
-        model.GuaranteedRatio = basetypes.NewFloat64Value(float64(*sdk.GuaranteedRatio))
-        tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "GuaranteedRatio", "value": *sdk.GuaranteedRatio})
-    } else {
-        model.GuaranteedRatio = basetypes.NewFloat64Null()
-    }
-    // Handling Primitives
-    // Standard primitive packing
-    if sdk.Profile != nil {
-        model.Profile = basetypes.NewStringValue(*sdk.Profile)
-        tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "Profile", "value": *sdk.Profile})
-    } else {
-        model.Profile = basetypes.NewStringNull()
-    }
-diags.Append(d...)
+	tflog.Debug(ctx, "Entering pack helper for models.BandwidthAllocationsQos", map[string]interface{}{"sdk_struct": sdk})
+	diags := diag.Diagnostics{}
+	var model models.BandwidthAllocationsQos
+	var d diag.Diagnostics
+	// Handling Primitives
+	// Standard primitive packing
+	if sdk.Customized != nil {
+		model.Customized = basetypes.NewBoolValue(*sdk.Customized)
+		tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "Customized", "value": *sdk.Customized})
+	} else {
+		model.Customized = basetypes.NewBoolNull()
+	}
+	// Handling Primitives
+	// Standard primitive packing
+	if sdk.Enabled != nil {
+		model.Enabled = basetypes.NewBoolValue(*sdk.Enabled)
+		tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "Enabled", "value": *sdk.Enabled})
+	} else {
+		model.Enabled = basetypes.NewBoolNull()
+	}
+	// Handling Primitives
+	// Standard primitive packing
+	if sdk.GuaranteedRatio != nil {
+		model.GuaranteedRatio = basetypes.NewFloat64Value(float64(*sdk.GuaranteedRatio))
+		tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "GuaranteedRatio", "value": *sdk.GuaranteedRatio})
+	} else {
+		model.GuaranteedRatio = basetypes.NewFloat64Null()
+	}
+	// Handling Primitives
+	// Standard primitive packing
+	if sdk.Profile != nil {
+		model.Profile = basetypes.NewStringValue(*sdk.Profile)
+		tflog.Debug(ctx, "Packed primitive pointer", map[string]interface{}{"field": "Profile", "value": *sdk.Profile})
+	} else {
+		model.Profile = basetypes.NewStringNull()
+	}
+	diags.Append(d...)
 
-    obj, d := types.ObjectValueFrom(ctx, models.BandwidthAllocationsQos{}.AttrTypes(), &model)
+	obj, d := types.ObjectValueFrom(ctx, models.BandwidthAllocationsQos{}.AttrTypes(), &model)
 	tflog.Debug(ctx, "Final object to be returned from pack helper", map[string]interface{}{"object": obj})
-    diags.Append(d...)
-    tflog.Debug(ctx, "Exiting pack helper for models.BandwidthAllocationsQos", map[string]interface{}{"has_errors": diags.HasError()})
-    return obj, diags
+	diags.Append(d...)
+	tflog.Debug(ctx, "Exiting pack helper for models.BandwidthAllocationsQos", map[string]interface{}{"has_errors": diags.HasError()})
+	return obj, diags
 
 }
 
@@ -315,6 +301,3 @@ func packBandwidthAllocationsQosListFromSdk(ctx context.Context, sdks []deployme
 	tflog.Debug(ctx, "Exiting list pack helper for models.BandwidthAllocationsQos", map[string]interface{}{"has_errors": diags.HasError()})
 	return basetypes.NewListValueFrom(ctx, models.BandwidthAllocationsQos{}.AttrType(), data)
 }
-
-
-*/

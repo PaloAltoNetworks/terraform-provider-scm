23674cea9fc6f77f0f645da6fc71c334fb700c41
Dhiren Chotwani <dchotwani@paloaltonetworks.com>
2026-07-30 23:08:17 -0700

    actions fix

--- internal/provider/config_setup/actions.go
+++ internal/provider/config_setup/actions.go
@@ -7,14 +7,14 @@ import (
 // GetActions returns all actions for this package.
 func GetActions() []func() action.Action {
 	return []func() action.Action{
+		NewSnippetSnapshotDiffAction,
+		NewSnippetSnapshotCompareAction,
 		NewTrustValidationAction,
 		NewSnippetSnapshotUpdatesAction,
 		NewSnippetSnapshotPublishAction,
-		NewSnippetSnapshotDiffAction,
+		NewSnippetSnapshotConvertAction,
 		NewSnippetSnapshotSaveAction,
 		NewSharedSnippetsLoadAction,
 		NewSnippetSnapshotLoadAction,
-		NewSnippetSnapshotCompareAction,
-		NewSnippetSnapshotConvertAction,
 	}
 }
